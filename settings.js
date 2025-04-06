216fb2fbfeb87f8fc768954e7e89d42f99681d86
Yunglance <168911598+Yunglance@users.noreply.github.com>
2025-04-06 09:29:47 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -4,23 +4,23 @@ const chalk = require('chalk')
 //owmner v card
 global.ytname = "YT: ElitePro" //ur yt chanel name
 global.socialm = "GitHub: Elite-MD" //ur github or insta name
-global.location = "Port Harcourt, Nigeria" //ur location
+global.location = "mars,in SPACE" //ur location
 
 //new settings 
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = 'vice-lex-V2' //ur bot name
+global.ownernumber = '2348148694795' //ur owner number
+global.ownername = 'vice-lex-V2' //ur owner name
 global.websitex = "https://youtube.com/@elitepro-empire"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
 
-global.themeemoji = '👨🏼‍💻'
-global.wm = "ELITE-PRO-V2"
+global.themeemoji = '🗿'
+global.wm = "vice-lex-V2"
 global.botscript = 'https://github.com/Elite-MD/Elite-Pro-V2' //script link
 global.packname = "Sticker By"
-global.author = "ELITE-PRO-V2\n\n+2348109263390"
-global.creator = "2348109263390@s.whatsapp.net"
+global.author = "vice-lex-V2\n\n+2348148694795"
+global.creator = "2348148694795@s.whatsapp.net"
 global.xprefix = '.' //set any prefix 
-global.premium = ["2348109263390"] // Premium User
+global.premium = ["2348148694795"] // Premium User
 //Stop here. Thanks for choosing ELITEPRO.
 
 
